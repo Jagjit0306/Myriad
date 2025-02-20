8628dbc132099fd8f2c5a34bb9541c475b02ec27
Jagjit <146818104+Jagjit0306@users.noreply.github.com>
2025-02-20 23:51:44 +0530

    This is rad

--- myriad/lib/components/extras.dart
+++ myriad/lib/components/extras.dart
@@ -11,11 +11,34 @@ class Extras extends StatefulWidget {
 }
 
 class _ExtrasState extends State<Extras> {
+  List<String> enabledFeatures = [];
+
   @override
   void initState() {
     super.initState();
-    configurator();
+    getConfiguration();
   }
+
+  void getConfiguration() async {
+    final List<String> gotConfig = await configurator();
+    if (gotConfig.isNotEmpty) {
+      setState(() {
+        enabledFeatures = gotConfig;
+      });
+    }
+  }
+
+  Map<String, IconData> iconMap = {
+    "colorify": Icons.color_lens_outlined,
+    "hearify": Icons.hearing_outlined,
+    "vibraillify": Icons.drag_indicator_outlined,
+    "wheelify": Icons.accessibility_new_outlined,
+    "medify": Icons.medical_services_outlined,
+    "sightify": Icons.visibility_outlined,
+    "serenify": Icons.self_improvement_rounded,
+    "speakify": Icons.record_voice_over_outlined,
+    "voicify": Icons.loop_outlined,
+  };
 
   @override
   Widget build(BuildContext context) {
@@ -31,82 +54,97 @@ class _ExtrasState extends State<Extras> {
           mainAxisAlignment: MainAxisAlignment.end,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            if (enabledFeatures.isNotEmpty)
+              Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  ...enabledFeatures.map(
+                    (e) => ExtraButton(
+                      iconData: iconMap[e] ?? Icons.api,
+                      name: e.replaceAllMapped(
+                        RegExp(r'(^|\s)([a-z])'),
+                        (Match match) =>
+                            '${match.group(1)}${match.group(2)!.toUpperCase()}',
+                      ),
+                      path: "/extras/$e",
+                    ),
+                  ),
+                  ExtraButton(
+                    path: "/onboarding",
+                    iconData: Icons.star,
+                    name: "Onboarding",
+                    color: Colors.green,
+                  ),
+                  ExtraButton(
+                    path: "",
+                    customCallback: () {
+                      signOutFromGoogle();
+                      context.push("/auth");
+                    },
+                    iconData: Icons.logout,
+                    color: Colors.red,
+                    name: "Logout",
+                  ),
+                ],
+              ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExtraButton extends StatelessWidget {
+  String name;
+  String path;
+  IconData iconData;
+  Color? color;
+  VoidCallback? customCallback;
+  ExtraButton({
+    super.key,
+    required this.name,
+    required this.path,
+    required this.iconData,
+    this.color,
+    this.customCallback,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: customCallback ?? () => context.push(path),
+      child: Padding(
+        padding: const EdgeInsets.fromLTRB(40, 5, 40, 5),
+        child: Container(
+          decoration: BoxDecoration(
+              border: Border.all(
+                color: Theme.of(context).colorScheme.onSecondaryContainer,
+                width: 0.8,
+              ),
+              gradient: LinearGradient(
+                colors: [
+                  color ?? Theme.of(context).colorScheme.onSecondaryContainer,
+                  (color?.withAlpha(90) ?? Theme.of(context).colorScheme.onSecondaryContainer.withAlpha(90)),
+                  Theme.of(context).colorScheme.surface,
+                  Theme.of(context).colorScheme.onSecondaryContainer,
+                ],
+                transform: GradientRotation(3.54)
+              ),
+              borderRadius: BorderRadius.circular(18)),
+          child: Padding(
+            padding: const EdgeInsets.fromLTRB(40, 15, 40, 15),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                GestureDetector(
-                  onTap: () => context.push('/extras/colorify'),
-                  child: const Card(
-                    child: Text("Colorify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/hearify'),
-                  child: const Card(
-                    child: Text("Hearify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/wheelify'),
-                  child: const Card(
-                    child: Text("Wheelify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/medify'),
-                  child: const Card(
-                    child: Text("Medify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/serenify'),
-                  child: const Card(
-                    child: Text("Serenify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/Sightify'),
-                  child: const Card(
-                    child: Text("Sightify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/speakify'),
-                  child: const Card(
-                    child: Text("Speakify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/vibraillify'),
-                  child: const Card(
-                    child: Text("Vibraillify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/extras/voicify'),
-                  child: const Card(
-                    child: Text("Voicify"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () => context.push('/onboarding'),
-                  child: const Card(
-                    child: Text("Onboarding"),
-                  ),
-                ),
-                GestureDetector(
-                  onTap: () {
-                    signOutFromGoogle();
-                    context.push("/auth");
-                  },
-                  child: const Card(
-                    child: Text("LOGOUT"),
-                  ),
+                Icon(iconData, size: 35),
+                Text(
+                  name,
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.w400, letterSpacing: 1.4),
                 ),
               ],
             ),
-          ],
+          ),
         ),
       ),
     );
