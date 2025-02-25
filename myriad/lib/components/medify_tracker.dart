757b9b7a5a565debb7bfa56361cccb0f6a907a80
Jagjit <146818104+Jagjit0306@users.noreply.github.com>
2025-02-26 04:16:40 +0530

    UI updates

--- myriad/lib/components/medify_tracker.dart
+++ myriad/lib/components/medify_tracker.dart
@@ -11,6 +11,7 @@ class MedifyTracker extends StatefulWidget {
 
 class _MedifyTrackerState extends State<MedifyTracker> {
   final MedifyHistory medifyHistory = MedifyHistory();
+  final PageController _pageController = PageController(viewportFraction: 0.9);
   List<dynamic> records = [];
 
   @override
@@ -51,28 +52,50 @@ class _MedifyTrackerState extends State<MedifyTracker> {
   @override
   Widget build(BuildContext context) {
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         MyButton(
-          text: "Clear",
+          text: "Clear History",
           enabled: true,
           onTap: medifyHistory.clearData,
         ),
-        SizedBox(
-          height: 200,
-          child: records.isEmpty
-              ? const Center(child: CircularProgressIndicator())
-              : ListView.builder(
-                  scrollDirection: Axis.horizontal,
-                  itemCount: records.length,
-                  itemBuilder: (context, index) {
-                    return _MedifyRecordCard(
-                      item: records[index],
-                      recordIndex: index,
-                      onToggle: toggleMedicineStatus,
-                    );
-                  },
-                ),
+        Text(
+          'Medication History',
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Theme.of(context).colorScheme.inversePrimary,
+          ),
         ),
+        SizedBox(
+            height: 310,
+            child: records.isEmpty
+                ? const Center(child: CircularProgressIndicator())
+                : PageView.builder(
+                    controller: _pageController,
+                    itemCount: records.length,
+                    itemBuilder: (context, index) {
+                      return AnimatedBuilder(
+                        animation: _pageController,
+                        builder: (context, child) {
+                          double scale = 1.0;
+                          if (_pageController.position.haveDimensions) {
+                            double pageOffset = _pageController.page! - index;
+                            scale =
+                                (1 - (pageOffset.abs() * 0.2)).clamp(0.8, 1.0);
+                          }
+                          return Transform.scale(
+                            scale: scale,
+                            child: _MedifyRecordCard(
+                              item: records[index],
+                              recordIndex: index,
+                              onToggle: toggleMedicineStatus,
+                            ),
+                          );
+                        },
+                      );
+                    },
+                  )),
       ],
     );
   }
@@ -92,18 +115,19 @@ class _MedifyRecordCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.red,
+      color: Theme.of(context).colorScheme.onSecondaryContainer,
       margin: const EdgeInsets.all(8.0),
       child: Container(
-        width:
-            MediaQuery.of(context).size.width * 0.9, // Fixed width for the card
+        width: MediaQuery.of(context).size.width, // Fixed width for the card
         padding: const EdgeInsets.all(8.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
               item["date"],
-              style: const TextStyle(fontWeight: FontWeight.bold),
+              style: const TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
             ),
             const SizedBox(height: 8),
             Expanded(
@@ -143,37 +167,59 @@ class _MedifyRecordMedicine extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: const Color.fromARGB(255, 85, 85, 85),
+      color: Theme.of(context).colorScheme.onSecondary.withAlpha(130),
       margin: const EdgeInsets.only(bottom: 4),
       child: Padding(
         padding: const EdgeInsets.all(4.0),
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.center,
           children: [
             Text(
               med["medicineName"],
-              style: const TextStyle(fontWeight: FontWeight.bold),
+              style: const TextStyle(
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+                fontSize: 16,
+              ),
             ),
-            SizedBox(
-              height: 30, // Fixed height for the times list
-              child: ListView.builder(
-                scrollDirection: Axis.horizontal,
-                itemCount: med['times'].length,
-                itemBuilder: (context, index) {
-                  Map<String, bool> timing =
-                      Map<String, bool>.from(med['times'][index]);
-                  return Padding(
-                    padding: const EdgeInsets.only(right: 4),
-                    child: GestureDetector(
-                      onTap: () => onToggle(recordIndex, medicineIndex, index),
-                      child: Chip(
-                        label: Text(timing.keys.first),
-                        backgroundColor:
-                            timing.values.first ? Colors.green : Colors.grey,
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: SizedBox(
+                height: 30, // Fixed height for the times list
+                child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: med['times'].length,
+                  itemBuilder: (context, index) {
+                    Map<String, bool> timing =
+                        Map<String, bool>.from(med['times'][index]);
+                    return Padding(
+                      padding: const EdgeInsets.only(right: 4),
+                      child: GestureDetector(
+                        onTap: () =>
+                            onToggle(recordIndex, medicineIndex, index),
+                        child: Chip(
+                          label: Row(
+                            spacing: 5,
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            mainAxisSize: MainAxisSize.max,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Text(timing.keys.first, style: TextStyle(fontWeight: FontWeight.w500),),
+                              Icon(
+                                timing.values.first
+                                    ? Icons.check
+                                    : Icons.question_mark,
+                                size: 15,
+                              )
+                            ],
+                          ),
+                          backgroundColor:
+                              timing.values.first ? Colors.green : Color.fromARGB(255, 182, 39, 54),
+                        ),
                       ),
-                    ),
-                  );
-                },
+                    );
+                  },
+                ),
               ),
             )
           ],
